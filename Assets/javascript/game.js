d3c8fd6bc63a7325e45bd49dcd6e254b37fe96f0
Mike Smith <mike.smith1@earthlink.net>
2018-02-26 15:04:40 -0600

    playing around with sound effects

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -8,21 +8,44 @@
         
         $("#yourGoal").text(random);
 
+
+// winner/loser functions
+
         function winner() {
             alert("Woohoo! You Won!!");
+            $.playSound("Assets/audio/Blastwave_FX.mp3");
             wins++;
             $("#numberWins").text(wins);
-            reset();
-            console.log(wins);
+            // reset();
+            // console.log(wins);
         }
         
         function loser() {
             alert("Drats! You Lose!!");
+            $.playSound("Assets/audio/spring_hit_wobble.mp3");
             losses++;
             $("#numberLosses").text(losses);
-            reset();
-            console.log(losses);
+            // reset();
+            // console.log(losses);
         }
+
+        // Enable audio triggers
+
+        $.extend({
+            playSound: function () {
+                return $(
+                       '<audio class="sound-player" autoplay="autoplay" style="display:none;">'
+                         + '<source src="' + arguments[0] + '" />'
+                         + '<embed src="' + arguments[0] + '" hidden="true" autostart="true" loop="false"/>'
+                       + '</audio>'
+                     ).appendTo('body');
+            },
+            stopSound: function () {
+                $(".sound-player").remove();
+            }
+        });
+
+
 // assign values to crystal buttons
         
         var num1 = Math.floor(Math.random()*12+1);
@@ -36,6 +59,7 @@
             userTotal = userTotal + num1;
             console.log("New userTotal " + userTotal);
             $("#subTotal").text(userTotal);
+            $.playSound("Assets/audio/hit_twang1.mp3");
             gameOver();
         });
 
@@ -45,6 +69,7 @@
             userTotal = userTotal + num2;
             console.log("New userTotal " + userTotal);
             $("#subTotal").text(userTotal);
+            $.playSound("Assets/audio/hit_twang1.mp3");
             gameOver();
         });
 
@@ -54,6 +79,7 @@
             userTotal = userTotal + num3;
             console.log("New userTotal " + userTotal);
             $("#subTotal").text(userTotal);
+            $.playSound("Assets/audio/hit_twang1.mp3");
             gameOver();
         });
 
@@ -63,6 +89,7 @@
             userTotal = userTotal + num4;
             console.log("New userTotal " + userTotal);
             $("#subTotal").text(userTotal);
+            $.playSound("Assets/audio/hit_twang1.mp3");
             gameOver();
         });
 
